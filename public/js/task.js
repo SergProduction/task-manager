2451ffce760ff57792b384094d405353af82c401
serg <ss0sergey@gmail.com>
2017-06-19 15:42:36 +0300

    fix hello doc

--- public/js/task.js
+++ public/js/task.js
@@ -12177,7 +12177,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var testData = _tools.localStore.get() || {
   tasks: [{
     title: 'Hello!',
-    description: '### features\n- Write markdown style\n- Сreate child tasks\n- Tasks are stored in the local browser store\n\n\n[markdawn help (rus)](http://paulradzkov.com/2014/markdown_cheatsheet/)\n\n[markdawn help (en)](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)',
+    description: '### features\n\n- Write markdown style\n- Сreate child tasks\n- Tasks are stored in the local browser store\n- Special commands\n\n[markdawn help (rus)](http://paulradzkov.com/2014/markdown_cheatsheet/)\n\n[markdawn help (en)](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)\n\n#### special commands\n\n- ```/task "name"``` add childs tasks. Expample - ```/task "subtask"```',
     createDate: +new Date(),
     id: (0, _tools.guid)(),
     parent: false,
--- source/stores/task.js
+++ source/stores/task.js
@@ -8,7 +8,7 @@ const testData = localStore.get() || {
   tasks: [
     {
       title:'Hello!',
-      description: '### features\n- Write markdown style\n- Сreate child tasks\n- Tasks are stored in the local browser store\n\n\n[markdawn help (rus)](http://paulradzkov.com/2014/markdown_cheatsheet/)\n\n[markdawn help (en)](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)',
+      description: '### features\n\n- Write markdown style\n- Сreate child tasks\n- Tasks are stored in the local browser store\n- Special commands\n\n[markdawn help (rus)](http://paulradzkov.com/2014/markdown_cheatsheet/)\n\n[markdawn help (en)](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)\n\n#### special commands\n\n- ```/task "name"``` add childs tasks. Expample - ```/task "subtask"```',
       createDate: +new Date(),
       id: guid(),
       parent: false,
